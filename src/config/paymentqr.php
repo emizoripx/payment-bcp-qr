2732b27e562ceea8eb2e59fe290184fba0d34161
Marco Cuellar <mcuellar@ipxserver.com>
2021-03-02 09:32:46 -0400

    added env in cofig file

--- src/config/paymentqr.php
+++ src/config/paymentqr.php
@@ -2,16 +2,16 @@
 
 return [
     'bcp' => [
-        'host' => 'BCP_HOST',
-        'user' => 'BCP_USER',
-        'password' => 'BCP_PASSWORD',
-        'public_token' => 'BCP_PUBLIC_TOKEN',
-        'app_user_id' => 'BCP_APP_USER_ID',
-        'business_code' => 'BCP_BUSINESS_CODE',
-        'service_code' => 'BCP_SERVICE_CODE',
-        'certificate_password' => 'BCP_CERTIFICATE_PASSWORD',
-        'default_expiration' => 'BCP_DEFAULT_EXPIRATION',
+        'host' => env('BCP_HOST',null),
+        'user' => env('BCP_USER',null),
+        'password' => env('BCP_PASSWORD',null),
+        'public_token' => env('BCP_PUBLIC_TOKEN',null),
+        'app_user_id' => env('BCP_APP_USER_ID',null),
+        'business_code' => env('BCP_BUSINESS_CODE',null),
+        'service_code' => env('BCP_SERVICE_CODE',null),
+        'certificate_password' => env('BCP_CERTIFICATE_PASSWORD',null),
+        'default_expiration' => env('BCP_DEFAULT_EXPIRATION',null),
         // El certificado de debe tener el siguiente formato certificate.pem
-        'certificate_path' => 'CERTIFICATE_PATH'
+        'certificate_path' => env('CERTIFICATE_PATH,null')
     ]
 ];
